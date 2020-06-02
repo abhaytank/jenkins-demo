463671400d5f85c5015222b44de8d5b6e2e9c962
Abhay Tank <abhaytank64@gmail.com>
2020-06-02 07:01:43 +0530

    Created Node Server

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (_, res) => {
+  res.sendFile("index.html", { root: path.join(__dirname, "public") });
+});
+
+app.listen(port, () =>
+  console.log(`Jenkins app listening at http://localhost:${port}`)
+);
